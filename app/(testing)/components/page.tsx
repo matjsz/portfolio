f26c2f82db47f2e33c382f0616facb71dc035ac6
Matheus Silva <54675543+matjsz@users.noreply.github.com>
2024-09-14 17:04:38 -0300

    feat: first workings on neural network component

---
+++ app/(testing)/components/page.tsx
@@ -0,0 +1,10 @@
+import NetworkNode from "@/app/components/NeuralNetwork/NetworkNode";
+
+export default function Home() {
+    return (
+      <div className="">
+        <NetworkNode projectURL={'https://github.com/matjsz'} />
+      </div>
+    );
+  }
+  
